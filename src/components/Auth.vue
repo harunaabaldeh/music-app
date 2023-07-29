44cf5adeb87b96b3073daab4afcdd031af7c80b3
harunaabaldeh <harunaabaldeh@gmail.com>
2023-07-30 00:21:21 +0100

    added form validation package veevalidate

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -56,13 +56,14 @@
                         </button>
                     </form>
                     <!-- Registration Form -->
-                    <form v-show="tab === 'register'">
+                    <vee-form v-show="tab === 'register'" :validation-schema="schema">
                         <!-- Name -->
                         <div class="mb-3">
                             <label class="inline-block mb-2">Name</label>
-                            <input type="text"
+                            <vee-field type="text" name="name"
                                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                                 placeholder="Enter Name" />
+                            <ErrorMessage class="text-red-600" name="name" />
                         </div>
                         <!-- Email -->
                         <div class="mb-3">
@@ -110,7 +111,7 @@
                             class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700">
                             Submit
                         </button>
-                    </form>
+                    </vee-form>
                 </div>
             </div>
         </div>
@@ -119,19 +120,30 @@
 <script>
 import { mapState, mapWritableState } from "pinia"
 import useModalState from "@/stores/modal"
+import { ErrorMessage } from "vee-validate";
 
 export default {
     name: "Auth",
     data() {
         return {
-            tab: "login"
-        }
+            tab: "login",
+            schema: {
+                name: "required",
+                email: "",
+                age: "",
+                password: "",
+                confirm_password: "",
+                country: "",
+                tos: ""
+            }
+        };
     },
     computed: {
         ...mapState(useModalState, ["hiddenClass"]),
         ...mapWritableState(useModalState, {
             modalVisibility: "isOpen"
         })
-    }
+    },
+    components: { ErrorMessage }
 }
 </script>
